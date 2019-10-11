9d3a0e672216ddc7de54f034ce4093ef79eeef31
Sandilya Jandhyala <sandilya.j@go-jek.com>
2019-10-11 14:51:54 +0530

    Update project.clj with clojars deploy config
    - Also adds the description, url and license

--- project.clj
+++ project.clj
@@ -1,3 +1,13 @@
 (defproject tech.gojek/meajurements "3.0.0"
+  :description "A statsd client that supports tagged metrics in the dogstatsd format"
+  :url "https://github.com/gojekfarm/meajurements"
+  :license {:name "Apache License, Version 2.0"
+            :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [clj-statsd "0.4.0"]])
+                 [clj-statsd "0.4.0"]]
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                    :username      :env/clojars_username
+                                    :password      :env/clojars_password
+                                    :sign-releases false}]]
+  :profiles {:uberjar {:aot         :all
+                       :global-vars {*warn-on-reflection* true}}})
